3e2262b61e698a34d2ca11bd7a430058722e5445
Martin Sereinig <ms@usersnap.com>
2019-01-15 22:17:46 +0100

    take broker url from environment

--- consumer/striking_message/__init__.py
+++ consumer/striking_message/__init__.py
@@ -1,8 +1,11 @@
+import os
+
 import dramatiq
 
 from dramatiq.brokers.rabbitmq import RabbitmqBroker
 
-rabbitmq_broker = RabbitmqBroker(url='amqp://striking:password@rabbitmq:5672/messages')
+# initialize broker
+rabbitmq_broker = RabbitmqBroker(url=os.environ.get('RABBITMQ_BROKER_URL'))
 dramatiq.set_broker(rabbitmq_broker)
 
 # import the tasks
